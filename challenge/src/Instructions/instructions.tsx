da8bfb17cb0ed3b0f458beb36ca88eb9416f826c
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-04-24 15:14:49 -0300

    Add instructions component placeholder

---
+++ challenge/src/Instructions/instructions.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Image from "next/image";
+
+const Instructions = () => {
+  return (
+    <div id="header-logo-container" className="relative">
+      <Image
+        src="/softruck_logo.png"
+        alt="Softruck logo"
+        width={56}
+        height={56}
+        priority
+      />
+      <p>
+        This animation is related to a Softruck coding challenge. You can read
+        more about it on the repository instructions.
+      </p>
+    </div>
+  );
+};
+
+export default Instructions;
