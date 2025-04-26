3522a82dbc8702ccfbb6e2ea403987b5111cbf15
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-04-26 15:22:04 -0300

    Adds external links

--- challenge/src/app/layout.tsx
+++ challenge/src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import "../styles/main.scss";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Softruck Challenge - Lucca Romaniello",
@@ -13,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- challenge/src/components/Instructions/index.tsx
+++ challenge/src/components/Instructions/index.tsx
@@ -3,18 +3,46 @@ import Image from "next/image";
 
 const Instructions = () => {
   return (
-    <div>
-      <Image
-        src="/softruck_logo.png"
-        alt="Softruck logo"
-        width={56}
-        height={56}
-        priority
-      />
-      <p>
-        This animation is related to a Softruck coding challenge. You can read
-        more about it on the repository instructions.
-      </p>
+    <div className="instructions-content">
+      <div>
+        <div className="image-container">
+          <Image
+            src="/softruck_logo.png"
+            alt="Softruck logo"
+            className="image"
+            width={160}
+            height={80}
+            sizes="100%"
+            priority
+          />
+        </div>
+        <h1>Softruck Design Challenge</h1>
+        <p className="text-secondary">
+          The map animation is related to a Softruck coding challenge. You can
+          read the instructions in the main repository.
+        </p>
+        <div className="contact-container">
+          <span>
+            <a
+              href="https://github.com/luccaromaniello/softruck-challenge"
+              target="_blank"
+              className="link"
+            >
+              GitHub Repository
+            </a>
+          </span>
+          <span>
+            <a
+              href="https://www.linkedin.com/in/luccaromaniello/"
+              target="_blank"
+              className="link"
+            >
+              LinkedIn
+            </a>
+          </span>
+        </div>
+      </div>
+      <p className="text-tertiary">Designed and built by Lucca Romaniello</p>
     </div>
   );
 };
